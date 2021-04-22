7f0281f095510860b9f22139f666a46ec5a7baeb
Nijat Dursunlu <nicat.dursunlu@gmail.com>
2021-04-22 14:08:16 +0400

    Update task 1.sh

--- task 1.sh
+++ task 1.sh
@@ -1,5 +1,5 @@
 # Write script that compare entered number with 20. If number is equal, pass the answer to command
-line.
+# line.
 
 #! /bin/bash
 
@@ -7,13 +7,10 @@ echo -e "Please enter your number: "
 read number
 
 if [[ $number -eq 20 ]]
-<<<<<<< HEAD:task 1.sh
+
 then 
     echo  "$number is equal 20"
 else 
     echo "$number is not equal 20"
 fi
-=======
-then echo  "Number is $number"
-fi
->>>>>>> 7f3496bb2f05382234158ac492bef3c75ead7788:task 1.txt
+
